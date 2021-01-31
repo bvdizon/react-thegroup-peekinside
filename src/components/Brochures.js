fcc2b175c5fa5642427ef4e97a7a80d424c04c7f
briandizon <bvdizon@gmail.com>
2021-02-01 05:12:47 +0800

    added .env file

--- src/components/Brochures.js
+++ src/components/Brochures.js
@@ -3,8 +3,11 @@ import { useFetch } from '../hooks/useFetch';
 import Card from './Card';
 import Loading from './Loading';
 
-const gsheetID = '1z-rXlasV9g2PerZz8uiVtuCEmY192U0LyJA3oWyo9i8';
-const gsheetNum = '10';
+// const gsheetID = '1z-rXlasV9g2PerZz8uiVtuCEmY192U0LyJA3oWyo9i8';
+// const gsheetNum = '10';
+
+const gsheetID = process.env.REACT_APP_GSHEET_ID;
+const gsheetNum = process.env.REACT_APP_GSHEET_NUM;
 
 const url = `https://spreadsheets.google.com/feeds/list/${gsheetID}/${gsheetNum}/public/values?alt=json`;
 
