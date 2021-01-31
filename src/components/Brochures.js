8a8b510c6b3a4ec4636739e8aca80f840ca3a8ec
briandizon <bvdizon@gmail.com>
2021-01-31 09:00:04 +0800

    fixed Active Yes Filter

--- src/components/Brochures.js
+++ src/components/Brochures.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
+import Card from './Card';
 import Loading from './Loading';
 
 const gsheetID = '1z-rXlasV9g2PerZz8uiVtuCEmY192U0LyJA3oWyo9i8';
@@ -23,54 +23,9 @@ const Brochures = () => {
       <section className='thegroup-listings'>
         {data &&
           data.feed.entry.map((data) => {
-            return (
-              <article className='thegroup-card' key={data.gsx$alias.$t}>
-                <div className='thegroup-card__img'>
-                  <img
-                    src={data.gsx$thumbnail.$t}
-                    alt={`listing brochure - ${data.gsx$property.$t}`}
-                  />
-                  <div className='thegroup-agent'>
-                    <p>
-                      {data.gsx$name.$t} <span>{data.gsx$lastname.$t}</span>
-                    </p>
-                  </div>
-                  <div className='remaxfirst-logo'>
-                    <img
-                      src='/assets/images/remax-first-logo-with-balloon.png'
-                      alt=''
-                    />
-                  </div>
-                  <div className='thegroup-card__img--small'>
-                    <img src={`/assets/images/${data.gsx$photo.$t}`} alt='' />
-                  </div>
-                </div>
-                <div className='thegroup-card__content'>
-                  <h3>{data.gsx$property.$t}</h3>
-                  <div className='property-info'>
-                    <p>
-                      <i className='fa fa-bed'></i>
-                      <span>{data.gsx$beds.$t} Beds</span>
-                    </p>
-                    <p>
-                      <i className='fa fa-bath'></i>
-                      <span>{data.gsx$baths.$t} Baths</span>
-                    </p>
-                    <p>
-                      <i className='fa fa-arrows-alt'></i>
-                      <span>{data.gsx$sqft.$t} Sqft</span>
-                    </p>
-                    <p>
-                      <i className='fa fa-car'></i>
-                      <span>{data.gsx$parking.$t} Parking</span>
-                    </p>
-                  </div>
-                  <Link to={`/view/${data.gsx$alias.$t}`} className='btn'>
-                    View Brochure
-                  </Link>
-                </div>
-              </article>
-            );
+            if (data.gsx$active.$t === 'Yes') {
+              return <Card data={data} key={data.gsx$alias.$t} />;
+            }
           })}
       </section>
     </main>
---
+++ src/components/Card.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Card = ({ data }) => {
+  return (
+    <article className='thegroup-card' key={data.gsx$alias.$t}>
+      <div className='thegroup-card__img'>
+        <img
+          src={data.gsx$thumbnail.$t}
+          alt={`listing brochure - ${data.gsx$property.$t}`}
+        />
+        <div className='thegroup-agent'>
+          <p>
+            {data.gsx$name.$t} <span>{data.gsx$lastname.$t}</span>
+          </p>
+        </div>
+        <div className='remaxfirst-logo'>
+          <img src='/assets/images/remax-first-logo-with-balloon.png' alt='' />
+        </div>
+        <div className='thegroup-card__img--small'>
+          <img src={`/assets/images/${data.gsx$photo.$t}`} alt='' />
+        </div>
+      </div>
+      <div className='thegroup-card__content'>
+        <h3>{data.gsx$property.$t}</h3>
+        <div className='property-info'>
+          <p>
+            <i className='fa fa-bed'></i>
+            <span>{data.gsx$beds.$t} Beds</span>
+          </p>
+          <p>
+            <i className='fa fa-bath'></i>
+            <span>{data.gsx$baths.$t} Baths</span>
+          </p>
+          <p>
+            <i className='fa fa-arrows-alt'></i>
+            <span>{data.gsx$sqft.$t} Sqft</span>
+          </p>
+          <p>
+            <i className='fa fa-car'></i>
+            <span>{data.gsx$parking.$t} Parking</span>
+          </p>
+        </div>
+        <Link to={`/view/${data.gsx$alias.$t}`} className='btn'>
+          View Brochure
+        </Link>
+      </div>
+    </article>
+  );
+};
+
+export default Card;
